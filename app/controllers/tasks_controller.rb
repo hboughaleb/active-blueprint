cb74cceeab4d30edfda761f90cd5dca73f1dc221
Julia Rix <Juliarix@gmail.com>
2017-11-14 12:30:33 -0500

    added skeleton of actions for controllers

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+    def index         # GET /tasks
+  end
+
+  def show          # GET /tasks/:id
+  end
+
+  def new           # GET /tasks/new
+  end
+
+  def create        # POST /tasks
+  end
+
+  def edit          # GET /tasks/:id/edit
+  end
+
+  def update        # PATCH /tasks/:id
+  end
+
+  def destroy       # DELETE /tasks/:id
+  end
+end
