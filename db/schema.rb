9ce53a0a3108e1414e5d87357b72cd80bb3b88b6
Julia Rix <Juliarix@gmail.com>
2017-11-15 11:13:00 -0500

    Successfully added column to documents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114223609) do
+ActiveRecord::Schema.define(version: 20171115160002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171114223609) do
     t.string   "type"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "status"
     t.index ["task_id"], name: "index_documents_on_task_id", using: :btree
   end
 
