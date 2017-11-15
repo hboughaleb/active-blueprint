9cf3d9ce95dbf72ca6af6d87d9bc363dbcac8b52
Julia Rix <Juliarix@gmail.com>
2017-11-15 17:59:44 -0500

    created method, created user show page, created project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :budget, presence: true
+  # validates :budget, presence: true
   validates :address, presence: true
   validates :status, inclusion: { in: [ true, false ] }
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :projects, only: [ :create, :new ]
+    resources :users, only: [ :show ]
+    resources :projects, only: [ :create, :new, :show ]
 
   root to: 'pages#home'
 
